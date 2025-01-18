d015d370b7ebd2bf1c86df007ff3760ff5d0dbad
Dragodui <alghazaliaks123@gmail.com>
2025-01-18 16:37:20 +0100

    MYSQL Added

--- backend/database/mongo-db/index.js
+++ backend/database/mongo-db/index.js
@@ -1,5 +1,5 @@
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/todos-test-app')
-.then(() => console.log("connected to DB"))
+.then(() => console.log("connected to mongoDB"))
 .catch(err => console.log(err));
--- backend/database/my-sql/index.js
+++ backend/database/my-sql/index.js
@@ -5,7 +5,7 @@ const { User, Product, Order, Restaurant, Review } = require('./schemas');
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log('Connection to the database established successfully.');
+    console.log('Connected to mySQL.');
     // await sequelize.sync({ force: true }); 
     // console.log('All models were synchronized successfully.');
   } catch (error) {
